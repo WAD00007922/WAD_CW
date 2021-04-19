d784e507c2d7ef034eb21e3de17aa16772d9086e
WAD00007922 <78699573+WAD00007922@users.noreply.github.com>
2021-04-19 14:45:32 +0500

    Add files via upload
    Applied SPA(not full), API controllers, and SRP and DRY for API controllers

---
+++ CarsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRental.DAL;
+using CarRental.DAL.DBO;
+using CarRental.DAL.Repositories;
+
+namespace CarRental.Controllers //SRP and DRY applied for API Cars controller//
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly IRepository<Car> _carRepo;
+
+        public CarsController(IRepository<Car> carRepo)
+        {
+            _carRepo = carRepo;
+        }
+
+        // GET: api/Cars
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Car>>> GetCars()
+        {
+            return await _carRepo.GetAllAsync();
+        }
+
+        // GET: api/Cars/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Car>> GetCar(int id)
+        {
+            var car = await _carRepo.GetByIdAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return car;
+        }
+
+        // PUT: api/Cars/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCar(int id, Car car)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _carRepo.UpdateAsync(car);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cars
+        [HttpPost]
+        public async Task<ActionResult> PostCar(Car car)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _carRepo.CreateAsync(car);
+            
+            return CreatedAtAction("GetCar", new { id = car.Id }, car);
+        }
+
+        // DELETE: api/Cars/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+           
+            var car = await _carRepo.GetByIdAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            await _carRepo.DeleteAsync(id);
+            
+            return NoContent();
+        }
+
+        private bool CarExists(int id)
+        {
+            return _carRepo.Exists(id);
+        }
+    }
+}
---
+++ ClientsController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRental.DAL;
+using CarRental.DAL.DBO;
+using CarRental.DAL.Repositories;
+
+namespace CarRental.Controllers //SRP and DRY applied for API Clients controller//
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly IRepository<Client> _clientRepo;
+
+        public ClientsController(IRepository<Client> clientRepo)
+        {
+            _clientRepo = clientRepo;
+        }
+
+        // GET: api/Clients
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await _clientRepo.GetAllAsync();
+        }
+
+        // GET: api/Clients/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> GetClient(int id)
+        {
+            var client = await _clientRepo.GetByIdAsync(id);
+
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return client;
+        }
+
+        // PUT: api/Clients/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClient([FromRoute] int id, [FromBody] Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _clientRepo.UpdateAsync(client);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Clients
+        [HttpPost]
+        public async Task<ActionResult> PostClient(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _clientRepo.CreateAsync(client);
+
+            return CreatedAtAction("GetClient", new { id = client.Id }, client);
+        }
+
+        // DELETE: api/Clients/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClient(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var client = await _clientRepo.GetByIdAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            await _clientRepo.DeleteAsync(id);
+
+            return NoContent();
+        }
+
+        private bool ClientExists(int id)
+        {
+            return _clientRepo.Exists(id);
+        }
+    }
+}
